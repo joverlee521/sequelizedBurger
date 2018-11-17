d7f845b104c61577b7cf3cb63ae5ba9097ef0abd
J.Lee <joverlee521@gmail.com>
2018-11-17 14:08:33 -0800

    dropped force true

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(router);
 
 var db = require("./models");
 
-db.sequelize.sync({force: true}).then(function(){
+db.sequelize.sync().then(function(){
     app.listen(PORT, function(){
         console.log("Server is listening on: http://localhost:" + PORT);
     });
