a5263b3a178dbc870b9171a849ef702f002f963d
J.Lee <joverlee521@gmail.com>
2018-11-17 20:24:59 -0800

    added comments

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,38 +1,52 @@
+// Dependencies
 var express = require("express");
 var db = require("../models");
 var router = express.Router();
 
+// Get route to display all burgers
 router.get("/", function(req, res){
+    // Using sequelize findAll method
     db.Burger.findAll({
+        // Return data in alphabetical order
         order: [
             ["burger_name", "ASC"]
         ], 
+        // Include associated Customer data
         include: [db.Customer]
     }).then(function(dbBurger){
+        // Store returned data in handlebar object
         var hbsObject = {
             burgers: dbBurger
         };
+        // Render handlebar page including handlebar object
         res.render("index", hbsObject);
     });
 });
 
+// Post route to add new burgers
 router.post("/api/burgers", function(req,res){
+    // Using sequelize create method to create a persistent instance
     db.Burger.create({
         burger_name: req.body.burger_name,
         CustomerId: req.body.CustomerId
     }).then(function(dbBurger){
+        // Return data regarding new burger added in JSON object
         res.json(dbBurger.dataValues);
     });
 });
 
+// Update route to update burger values for specific burgers
 router.put("/api/burgers/:id", function(req, res){
+    // Using sequelize update method to update completed state 
     db.Burger.update({
         completed: req.body.completed
     }, {
+        // Specifies id to update
         where: {
             id: req.params.id
         }
     }).then(function(dbBurger){
+        // If nothing was changed, return 404 error
         if(dbBurger === 0){
             return res.status(404).end();
         }
@@ -40,12 +54,16 @@ router.put("/api/burgers/:id", function(req, res){
     });
 });
 
+// Delete route to delete burger 
 router.delete("/api/burgers/:id", function(req, res){
+    // Using sequelize destory method to delete burger 
     db.Burger.destroy({
+        // Specifies id to delete
         where: {
             id: req.params.id
         }
     }).then(function(dbBurger){
+        // If nothing was deleted, return 404 error
         if(dbBurger === 0){
             return res.status(404).end();
         }
@@ -53,17 +71,21 @@ router.delete("/api/burgers/:id", function(req, res){
     });
 });
 
+// Post route to add new customer
 router.post("/api/customers", function(req, res){
+    // Using sequelize to find customer or create new one if does not exist
     db.Customer.findOrCreate({
         where: {
             name: req.body.name
         }
+        //Divides returned array into data and created boolean that are passed into callback function 
     }).spread(function(dbCustomer, created){
         if(created){
             console.log("New Customer Created!");
         }else{
             console.log("Existing Customer");
         }
+        // return new customer data as JSON object
         res.json(dbCustomer.dataValues);
     });
 });
--- server.js
+++ server.js
@@ -1,22 +1,31 @@
+// External Dependencies
 var express = require("express");
+var path = require("path");
+var exphbs = require("express-handlebars");
+
+// Setting up the express app
 var app = express();
 var PORT = process.env.PORT || 8080;
 
+// Middleware to handle data parsing in express app
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-var path = require("path");
+// Static directory
 app.use(express.static(path.join(__dirname, "/public")));
 
-var exphbs = require("express-handlebars");
+// Middleware to set up handlebars for express app
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+// Routes
 var router = require("./controllers/burgers_controller");
 app.use(router);
 
+// Requiring models for syncing
 var db = require("./models");
 
+// Sync sequelize models and start app
 db.sequelize.sync().then(function(){
     app.listen(PORT, function(){
         console.log("Server is listening on: http://localhost:" + PORT);
